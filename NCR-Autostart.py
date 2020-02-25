db1e692cb2571568241b645c027fc963c1898456
Kascique Lowmans <kascique.lowmans@gmail.com>
2020-02-25 15:54:36 -0400

    Update NCR-Autostart.py

--- NCR-Autostart.py
+++ NCR-Autostart.py
@@ -1,5 +1,12 @@
-#NCR Counterpoint autostart
+#NCR Counterpoint auto login
 
 import subprocess
 import win32api
 import win32com.client
+
+counterpoint = 'C:\Program Files (x86)\Radiant Systems\CounterPoint\CPSQL.1 - Station\Bin\CounterPoint.exe'
+subprocess.Popen(counterpoint)
+shell = win32com.client.Dispatch("WScript.shell")
+win32api.Sleep(6000)
+shell.SendKeys("laynes123")
+shell.SendKeys("{ENTER}")
